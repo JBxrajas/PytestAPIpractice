dd6b7669313d918fd42d73b5350a0c146bb921ea
jesus.barajas <jesus_barajas@epam.com>
2026-02-20 13:26:55 -0600

    more tests and refactor first test

--- test_todo_api.py
+++ test_todo_api.py
@@ -1,15 +1,43 @@
 import requests
 ENDPOINT = "http://todo.pixegami.io"
 
-response = requests.get(ENDPOINT)
-status_code = response.status_code
-print(response.json())
-print(status_code)
-
 def test_get_todo():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
     pass
 
 
-test_get_todo()
+def test_create_task():
+    payload = new_task_payload()
+    create_task_response= create_task(payload)
+    assert create_task_response.status_code == 200
+    data = create_task_response.json()
+    print(data)
+
+    task_id = data["task"]["task_id"]  # Changed from data["task"][task_id]
+    create_task_response = requests.get(ENDPOINT + f"/get-task/{task_id}")
+    assert create_task_response.status_code == 200
+    get_task_data = create_task_response.json()
+
+    assert get_task_data["content"] == payload["content"]  
+    assert get_task_data["user_id"] == payload["user_id"]
+
+
+def test_can_update_task():
+    # First, create a task to update
+    #update task
+    #get validate the changes
+    pass
+
+def create_task(payload):
+    return requests.put(ENDPOINT + "/create-task", json=payload)
+
+def get_task(task_id):
+    return requests.get(ENDPOINT + f"/get-task/{task_id}")
+
+def new_task_payload():
+    return {
+        "content": "Test Api Task",
+        "user_id": "test_user",
+        "is_done": False
+    }
